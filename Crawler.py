df8211c223c8091f509aac2cca6066f379ba7f0b
Vinicius Faria <artixsproductions@gmail.com>
2025-04-12 15:13:10 -0300

    feat: added stricter url parsing, moved calling crawlable to when url is actually obtained from frontier

--- Crawler.py
+++ Crawler.py
@@ -7,6 +7,16 @@ from time import time
 from Corpus import Corpus
 from urllib.parse import urlparse
 from protego import Protego
+import re
+
+#Taken from https://github.com/django/django/blob/main/django/core/validators.py
+url_regex = re.compile(
+        r'^(?:http)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 class Crawler:
     """Base crawler class, responsible for basic crawling and managing similar structures. 
@@ -27,15 +37,15 @@ class Crawler:
 
         for s in seeds:
             s = url_normalize(s) #Basic normalization, benefit of the doubt that seeds are simple urls
-            if (self.url_crawlable(s)):
-                self.frontier.put(s)
-                self.visited.add(s)
+            self.frontier.put(s)
+            self.visited.add(s)
     
     def crawl(self):
-        #TODO abide to robots.txt, expand frontier, paralellize, etc
         while not self.frontier.empty() and self.crawled < self.to_crawl:
 
-            url = self.frontier.get()
+            url = self.frontier.get() #Assumes crawl-delay is taken into account by frontier
+            if not self.url_crawlable(url): #Will check robots.txt allow/disallow
+                continue
 
             try:
                 res = requests.get(url, stream=False)
@@ -71,7 +81,12 @@ class Crawler:
 
         self.corpus.close()
     
-    def process_outlinks(self, url, soup):
+    def process_outlinks(self, url: str, soup: BeautifulSoup) -> None:
+        """Processes outlinks parsed in all <a> tags in parsed structure, while also checking for url malformation and invalid protocols.
+        
+        Handles malformatted urls, relative urls and protocols, while also performing url normalization. Only HTTP/HTTPS protocols are allowed.
+        """
+
         #Expand queue by finding links
         for tag in soup.find_all('a'):
             link = tag.get('href')
@@ -79,14 +94,19 @@ class Crawler:
             #Skip empty/missing href and hashes
             if link is None or link == '' or link[0] == '#': continue
 
-            try:
-                normal = url_normalize(link, default_domain=url, filter_params=True)
-                normal = normal.split('#', 1)[0] #Remove hashes # in links too
+            #Handle relative url + relative protocols (url_normalize doesn't handle this well...)
+            if link[0:2] == '//':
+                link = f"{urlparse(url).scheme}://{link}"
+            elif link[0] == '/':
+                link = url + link
 
+            try:
+                normal = url_normalize(link, filter_params=True)
+                normal = normal.split('#', 1)[0] #Remove hashes # in links too
             except: #Couldnt parse url, probably not an url in the first place...
                 continue
-
-            if not self.url_crawlable(normal):
+                
+            if re.match(url_regex, normal) == None: #Final check for URL malformation
                 continue
             
             if normal not in self.visited: #Revisitation policy
@@ -94,22 +114,30 @@ class Crawler:
                 self.visited.add(normal)
     
     def url_crawlable(self, url: str) -> bool:
-        """Checks if url is crawlable given host's robot.txt. Only checks for allow/disallow tags.
-        Does not take into account crawl delay, as that is handled by the frontier."""
+        """Checks if url is crawlable given host's robot.txt. Also checks for URL malformation.
+        Does not take into account crawl delay, as that is handled by the frontier.
+        
+        Only fetches according `robots.txt` if not yet in memory."""
 
         parsed = urlparse(url)
         origin = f"{parsed.scheme}://{parsed.netloc}"
 
-        if origin in self.host2robots:
+        #Quick check for malformed or not in http/https
+        if not (parsed.scheme in {'http', 'https'} and bool(parsed.netloc)): 
+            return False
+
+        if origin in self.host2robots: #Check local
             val = self.host2robots[origin]
 
             if type(val) == float: return True #robots not found, no link restrictions
-            return self.host2robots[origin].can_fetch(url, '')
-        
-        #Look for robots.txt
+            return val.can_fetch(url, '')
+
+        #Gotta fetch
         try:
-            resp = requests.get(f"{origin}/robots.txt")
-        except: #Too much can go wrong here, just assume it doesn't exist
+            resp = requests.get(f"{origin}/robots.txt", timeout=1) #Tighter timeout for robots
+            resp.raise_for_status()
+
+        except requests.RequestException: #Website does not have robots.txt or not responding
             self.host2robots[origin] = self.default_delay
             return True
         
