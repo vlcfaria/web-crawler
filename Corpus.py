12e400cf46f6408161f383504d8be00599d17f30
Vinicius Faria <artixsproductions@gmail.com>
2025-04-14 16:03:31 -0300

    feat: added simple locks to corpus

--- Corpus.py
+++ Corpus.py
@@ -3,12 +3,13 @@ from warcio.warcwriter import WARCWriter
 from warcio.statusandheaders import StatusAndHeaders
 from io import BytesIO
 import requests
+import threading
 
 class Corpus:
     """Class responsible for writing WARC format entries, given request response, storing into local storage.
 
     Pages are split into separate files according to `pages_ratio`.
-    Files are then stored as `"target_directory/base_name-xxxx.gz"`"""
+    Files are then stored as `"target_directory/base_name-xxxx.warc.gz"`"""
     def __init__(self, target_directory: str, base_name='pages', pages_ratio=1000):
         
         self.target_directory = target_directory
@@ -19,38 +20,42 @@ class Corpus:
         
         self.file_num = 1
         self.count = 0
-        self.cur_file = open(f"{self.target_directory}/{self.base_name}-{self.file_num}.gz", 'wb')
+        self.cur_file = open(f"{self.target_directory}/{self.base_name}-{self.file_num}.warc.gz", 'wb')
         self.writer = WARCWriter(self.cur_file, gzip=True)
+
+        self.lock = threading.Lock()
     
     def _next_file(self) -> None:
-        'Closes current file, increments file counter and opens new .warc file for future writing'
+        'Lock before calling this! Closes current file, increments file counter and opens new .warc file for future writing.'
         self.cur_file.close()
         self.count = 0
         self.file_num += 1
 
         #Refresh file and writer
-        self.cur_file = open(f"{self.target_directory}/{self.base_name}-{self.file_num}.warc", 'wb')
+        self.cur_file = open(f"{self.target_directory}/{self.base_name}-{self.file_num}.warc.gz", 'wb')
         self.writer = WARCWriter(self.cur_file, gzip=True)
     
     def write(self, url: str, resp: requests.Response) -> None:
         'Takes in raw response content and append to current file, swapping if necessary'
-        #Get next file if needed
-        if self.count == self.pages_ratio:
-            self._next_file()
-        
-        #Create record
-        headers_list = resp.raw.headers.items()
-        http_headers = StatusAndHeaders('200 OK', headers_list, protocol='HTTP/1.0')
+        with self.lock:
+            #Get next file if needed
+            if self.count == self.pages_ratio:
+                self._next_file()
+            
+            #Create record
+            headers_list = resp.raw.headers.items()
+            http_headers = StatusAndHeaders('200 OK', headers_list, protocol='HTTP/1.0')
 
-        #warcio expects a stream, so convert content to stream
-        record = self.writer.create_warc_record(url, 'response',
-                                                payload=BytesIO(resp.content), http_headers=http_headers)
-        
-        #Store & increment
-        self.writer.write_record(record)
-        self.count += 1
+            #warcio expects a stream, so convert content to stream
+            record = self.writer.create_warc_record(url, 'response',
+                                                    payload=BytesIO(resp.content), http_headers=http_headers)
+            
+            #Store & increment
+            self.writer.write_record(record)
+            self.count += 1
     
     def close(self) -> None:
         """Closes current file, if not yet closed."""
-        if not self.cur_file.closed:
-            self.cur_file.close()
+        with self.lock:
+            if not self.cur_file.closed:
+                self.cur_file.close()
